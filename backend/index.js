65bf366719672386039b6e358c52328354aaa867
dammu kiran <dammukiran4@gmail.com>
2025-05-18 22:48:59 +0530

    get

--- backend/index.js
+++ backend/index.js
@@ -2,27 +2,27 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import { connectDB } from "./src/utils/db.js";
-
 import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
 import { app, server } from "./src/utils/socket.js";
 
 dotenv.config();
+//connect to database
 connectDB();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
-app.use(
-  cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({origin: "http://localhost:5173",credentials: true}));
 
+//routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))};
+app.get('/',(req,res)=>{
+  res.send(<h1>Hello Dammu Chat App</h1>)
+})
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000
