db4b9705a10e101f2da4b8a228643131d007422d
dammu kiran <dammukiran4@gmail.com>
2025-05-18 22:03:42 +0530

    vercel 2

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,7 @@ import messageRoutes from "./src/routes/message.route.js";
 import { app, server } from "./src/utils/socket.js";
 
 dotenv.config();
-
-const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
@@ -26,9 +25,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))};
 
 if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000
   server.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
-    connectDB();
+    
   });
 }
 
