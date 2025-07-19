b600782604e275eb82bc87e113f57c1c8194a674
Dammu Kiran <159158720+kirandammu@users.noreply.github.com>
2025-07-19 21:44:00 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://dammu-chat.vercel.app/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://dammu-chat-2.onrender.com/api" : "/api",
   withCredentials: true,
 });
