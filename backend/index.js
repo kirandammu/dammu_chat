1fdcb205bfdfd2bd32fbdc8eb1d7a75381bf74a5
Dammu Kiran <159158720+kirandammu@users.noreply.github.com>
2025-07-19 21:38:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({origin: "https://dammu-chat.vercel.app",credentials: true}));
 
 //routes
 app.use("/api/auth", authRoutes);
