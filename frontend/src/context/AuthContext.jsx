692420e46347b636053d79766341ced5bb900be7
Dammu Kiran <159158720+kirandammu@users.noreply.github.com>
2025-07-19 21:28:15 +0530

    Update AuthContext.jsx
    online

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
+const BASE_URL = "https://dammu-chat-2.onrender.com" || "http://localhost:5000"
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
